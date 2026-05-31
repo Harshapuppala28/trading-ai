6ac4433aece2997756ec0e7e9f474b7a262b8c85
Harsha <puppalaharsha2805@gmail.com>
2026-05-31 13:20:55 +0530

    Added Flask wrapper for Render

---
+++ app.py
@@ -0,0 +1,52 @@
+
+from flask import Flask
+import threading
+import os
+
+app = Flask(__name__)
+
+
+# ==========================================
+# HOME ROUTE
+# ==========================================
+
+@app.route("/")
+def home():
+
+    return "Trading AI Scanner Running 🚀"
+
+
+# ==========================================
+# START SCANNER
+# ==========================================
+
+def run_scanner():
+
+    os.system("python data_fetcher.py")
+
+
+scanner_thread = threading.Thread(
+    target=run_scanner
+)
+
+scanner_thread.start()
+
+
+# ==========================================
+# RUN FLASK
+# ==========================================
+
+if __name__ == "__main__":
+
+    port = int(
+        os.environ.get("PORT", 10000)
+    )
+
+    app.run(
+
+        host="0.0.0.0",
+
+        port=port
+
+    )
+
